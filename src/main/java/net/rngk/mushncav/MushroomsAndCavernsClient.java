9d85855f73fc8be0802fa03e15225ffd6d872eb7
rngkGit <Kehaw06@gmail.com>
2024-02-20 22:44:53 -0600

    Initial commit

---
+++ src/main/java/net/rngk/mushncav/MushroomsAndCavernsClient.java
@@ -0,0 +1,10 @@
+package net.rngk.mushncav;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MushroomsAndCavernsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
