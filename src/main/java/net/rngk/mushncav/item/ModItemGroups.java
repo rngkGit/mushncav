8cd56e50d41e864765d5a2082fd08ff0e52398e1
rngkGit <Kehaw06@gmail.com>
2024-02-21 13:09:17 -0600

    Added some basic items w/o textures

--- src/main/java/net/rngk/mushncav/item/ModItemGroups.java
+++ src/main/java/net/rngk/mushncav/item/ModItemGroups.java
@@ -13,7 +13,15 @@ public class ModItemGroups {
     public static final ItemGroup MUSHNCAVE_GROUP = Registry.register(Registries.ITEM_GROUP, new Identifier(MushroomsAndCaverns.MOD_ID, "mushncav"),
             FabricItemGroup.builder().displayName(Text.translatable("itemgroup.mushncav"))
                     .icon(() -> new ItemStack(ModItems.MAGIC_STONE)).entries((displayContext, entries) -> {
+                        //In order
                         entries.add(ModItems.MAGIC_STONE);
+                        entries.add(ModItems.FUNGI_BONE);
+                        entries.add(ModItems.MUSHROOM_BONE);
+                        entries.add(ModItems.FUNGI_STICK);
+                        entries.add(ModItems.AMETHYST_MUSHROOM_STICK);
+                        entries.add(ModItems.GLOWING_MUSHROOM_STICK);
+                        entries.add(ModItems.GLOWING_SAPHIRE_SHARD);
+                        entries.add(ModItems.GLOWING_SAPHIRE_CRYSTAL);
                     }).build());
     public static void registerItemGroups(){
         MushroomsAndCaverns.LOGGER.info("Registering item groups for " + MushroomsAndCaverns.MOD_ID);
--- src/main/java/net/rngk/mushncav/item/ModItems.java
+++ src/main/java/net/rngk/mushncav/item/ModItems.java
@@ -1,23 +1,36 @@
 package net.rngk.mushncav.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+//import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+//import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
+//import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.rngk.mushncav.MushroomsAndCaverns;
 
 public class ModItems {
+    //Items (unorganized, I will do that later)
+    public static final Item FUNGI_STICK = registerItem("fungistick", new Item(new FabricItemSettings()));
+    public static final Item AMETHYST_MUSHROOM_STICK = registerItem("amethystmushroomstick", new Item(new FabricItemSettings()));
+    public static final Item GLOWING_MUSHROOM_STICK = registerItem("glowingmushroomstick", new Item(new FabricItemSettings()));
+    //public static final Item FUNGI_APPLE = registerItem("fungiapple", new Item(new FabricItemSettings()));
+    public static final Item FUNGI_BONE = registerItem("fungibone", new Item(new FabricItemSettings()));
+    public static final Item MUSHROOM_BONE = registerItem("mushroombone", new Item(new FabricItemSettings()));
+    public static final Item GLOWING_SAPHIRE_SHARD = registerItem("glowingsaphireshard", new Item(new FabricItemSettings()));
+    public static final Item GLOWING_SAPHIRE_CRYSTAL = registerItem("glowingsaphirecrystal", new Item(new FabricItemSettings()));
     public static final Item MAGIC_STONE = registerItem("magicstone", new Item(new FabricItemSettings()));
+
     /*private static void addItemsToIngredientTab(FabricItemGroupEntries entries){
+        //add stuff to the existing ingredients tab, this code is for reference and probally wont be used ever, but Ill leave it here.
         entries.add(MAGIC_STONE);
     }*/
+
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, new Identifier(MushroomsAndCaverns.MOD_ID, name), item);
     }
+
     public static void registerModItems(){
         MushroomsAndCaverns.LOGGER.info("Registering mod items for " + MushroomsAndCaverns.MOD_ID);
 
